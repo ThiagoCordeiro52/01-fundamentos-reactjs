689dc4c134fa36259e7cb3bd483fc7f95a0af28f
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-09-11 16:37:00 -0300

    :sparkles: feat: sidebar styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { Header } from "./components/Header";
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis dignissimos quaerat ipsum repellat sit aperiam asperiores, cumque ad! Voluptas unde ex doloremque maiores assumenda temporibus animi optio autem quisquam!"
+            />
+
+            <Post 
+            author="Thiago Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis reiciendis dignissimos quaerat ipsum repellat sit aperiam asperiores, cumque ad! Voluptas unde ex doloremque maiores assumenda temporibus animi optio autem quisquam!"
+            />
+
+        </main>
+      </div>
     </div>
   )
 }
+
 
