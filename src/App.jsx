d3bb5a59dfaaf6caa9e7a0d876376743be1bf0d9
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-08-10 19:58:40 -0300

    :sparkles: feat: CSS modules added to header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Post from './Post'
+import { Header } from './components/Header'
+
+import './styles.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Post 
+        author="Thiago" content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt assumenda, a voluptate maiores repellat? Numquam, hic? Atque cupiditate quo, aperiam quas vel odit harum veniam voluptatibus! Eaque, suscipit rem.
+        " 
+      />
+    </div>
+  )
+}
+
+export default App
+
