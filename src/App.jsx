ecf9b2b6291dea9ed65b85967e2f02d532c82052
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-08-14 11:27:25 -0300

    :sparkles: feat: added sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import Post from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Thiago" content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt assumenda, a voluptate maiores repellat? Numquam, hic? Atque cupiditate quo, aperiam quas vel odit harum veniam voluptatibus! Eaque, suscipit rem.
-        " 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Thiago" content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt assumenda, a voluptate maiores repellat? Numquam, hic? Atque cupiditate quo, aperiam quas vel odit harum veniam voluptatibus! Eaque, suscipit rem.
+          " 
+          />
+        </main>
+      </div>
     </div>
   )
 }
