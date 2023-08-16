5e1764ce2740dde6a0730a1cd2131fb0d0664c4d
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-08-16 14:47:23 -0300

    :sparkles: feat: added styles to post component and added a new color in global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Post from './Post'
-import { Header } from './components/Header'
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-import './global.css'
-import { Sidebar } from './components/Sidebar'
+import "./global.css";
 
 function App() {
   return (
@@ -13,15 +13,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Thiago" content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt assumenda, a voluptate maiores repellat? Numquam, hic? Atque cupiditate quo, aperiam quas vel odit harum veniam voluptatibus! Eaque, suscipit rem.
-          " 
+          <Post
+            author="Thiago"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt assumenda, a voluptate maiores repellat? Numquam, hic? Atque cupiditate quo, aperiam quas vel odit harum veniam voluptatibus! Eaque, suscipit rem.
+          "
           />
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
